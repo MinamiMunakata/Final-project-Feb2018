9ec6a87739d4002fb54d0bb4546e36738d3e24ff
Minami Munakata <june28munazo@gmail.com>
2018-02-15 17:52:25 -0800

    first init

---
+++ js/app.js
@@ -0,0 +1,18 @@
+$(function() {
+$('.pie_chart').easyPieChart({
+//ここに各設定パラメータを記述していきます。
+	easing:'easeOutQuart',
+	barColor:'rgba(255,109,97,1.0)',
+	trackColor:'rgba(255,218,201,1.0)',
+	lineCap:'square',
+	lineWidth: 20,
+	trackWidth: 16,
+	scaleLength:10,
+	scaleColor:false,
+	animate:{ duration: 5000, enabled: true },
+	rotate:30,
+	onStep: function(from, to, percent) {
+	$(this.el).find('.pie_percent').text(Math.round(percent));
+	}
+});
+});
